7899f4d20c91e0810da1c53b73d039804264cf29
Zachary Yedidia <zyedidia@gmail.com>
2026-01-21 17:35:59 -0500

    [LFI] Add MCLFIRewriter infrastructure

--- llvm/lib/MC/MCStreamer.cpp
+++ llvm/lib/MC/MCStreamer.cpp
@@ -406,6 +406,9 @@ void MCStreamer::emitLabel(MCSymbol *Symbol, SMLoc Loc) {
 
   Symbol->setFragment(&getCurrentSectionOnly()->getDummyFragment());
 
+  if (LFIRewriter)
+    LFIRewriter->onLabel(Symbol);
+
   MCTargetStreamer *TS = getTargetStreamer();
   if (TS)
     TS->emitLabel(Symbol);
