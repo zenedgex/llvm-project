97417137a39a43a83328fe5140ffb6bd79bab0a6
chinmayingle <chinmay.r.ingle@gmail.com>
2026-01-27 13:00:55 +0530

    Address review comments

--- libc/src/math/generic/hypot.cpp
+++ libc/src/math/generic/hypot.cpp
@@ -8,15 +8,11 @@
 
 #include "src/math/hypot.h"
 #include "src/__support/math/hypot.h"
-// #include "src/__support/FPUtil/Hypot.h"
-// #include "src/__support/common.h"
-// #include "src/__support/macros/config.h"
 
 namespace LIBC_NAMESPACE_DECL {
 
 LLVM_LIBC_FUNCTION(double, hypot, (double x, double y)) {
   return math::hypot(x, y);
-  // return LIBC_NAMESPACE::fputil::hypot(x, y);
 }
 
 } // namespace LIBC_NAMESPACE_DECL
