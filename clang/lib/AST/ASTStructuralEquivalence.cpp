f36d41b906bd392b9502e30fbe7918545790fd06
Yeoul Na <yeoul_na@apple.com>
2026-02-07 09:18:05 -0800

    clang-format

--- clang/lib/AST/ASTStructuralEquivalence.cpp
+++ clang/lib/AST/ASTStructuralEquivalence.cpp
@@ -1149,9 +1149,9 @@ static bool IsStructurallyEquivalent(StructuralEquivalenceContext &Context,
     break;
 
   case Type::LateParsedAttr:
-    if (!IsStructurallyEquivalent(Context,
-                                  cast<LateParsedAttrType>(T1)->getWrappedType(),
-                                  cast<LateParsedAttrType>(T2)->getWrappedType()))
+    if (!IsStructurallyEquivalent(
+            Context, cast<LateParsedAttrType>(T1)->getWrappedType(),
+            cast<LateParsedAttrType>(T2)->getWrappedType()))
       return false;
     break;
 
--- clang/lib/Sema/TreeTransform.h
+++ clang/lib/Sema/TreeTransform.h
@@ -7731,8 +7731,9 @@ QualType TreeTransform<Derived>::TransformCountAttributedType(
 }
 
 template <typename Derived>
-QualType TreeTransform<Derived>::TransformLateParsedAttrType(
-    TypeLocBuilder &TLB, LateParsedAttrTypeLoc TL) {
+QualType
+TreeTransform<Derived>::TransformLateParsedAttrType(TypeLocBuilder &TLB,
+                                                    LateParsedAttrTypeLoc TL) {
   const LateParsedAttrType *OldTy = TL.getTypePtr();
   QualType InnerTy = getDerived().TransformType(TLB, TL.getInnerLoc());
   if (InnerTy.isNull())
