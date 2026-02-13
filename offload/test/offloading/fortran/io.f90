16184541ea3b7d878548e1a1fb8de07f609e7429
Joseph Huber <huberjn@outlook.com>
2026-02-12 22:06:10 -0600

    update test

--- offload/test/offloading/fortran/io.f90
+++ offload/test/offloading/fortran/io.f90
@@ -1,10 +1,7 @@
 ! REQUIRES: flang, libc
 ! RUN: %libomptarget-compile-fortran-run-and-check-generic
 
-! REQUIRES: flang, libc
-! RUN: %libomptarget-compile-fortran-run-and-check-generic
-
-program hello_gpu
+program io_test
   implicit none
 
   integer :: i
@@ -17,42 +14,14 @@ program hello_gpu
   c = (1.0, -1.0)
   l = .true.
 
-  ! CHECK: Hello from GPU
-  ! CHECK: Hello from GPU
-  ! CHECK: Hello from GPU
-  ! CHECK: Hello from GPU
+  ! CHECK: Text 42 3.14 (1.,-1.) T
+  ! CHECK: Text 42 3.14 (1.,-1.) T
+  ! CHECK: Text 42 3.14 (1.,-1.) T
+  ! CHECK: Text 42 3.14 (1.,-1.) T
   !$omp target teams num_teams(4)
   !$omp parallel num_threads(1)
-    print *, "Hello from GPU"
+    print *, "Text", " ", i, " ", r, " ", c, " ", l
   !$omp end parallel
   !$omp end target teams
 
-  ! CHECK: 42
-  !$omp target teams num_teams(1)
-  !$omp parallel num_threads(1)
-    print *, i
-  !$omp end parallel
-  !$omp end target teams
-
-  ! CHECK: 3.14
-  !$omp target teams num_teams(1)
-  !$omp parallel num_threads(1)
-    print *, r
-  !$omp end parallel
-  !$omp end target teams
-
-  ! CHECK: (1.,-1.)
-  !$omp target teams num_teams(1)
-  !$omp parallel num_threads(1)
-    print *, c
-  !$omp end parallel
-  !$omp end target teams
-
-  ! CHECK: T
-  !$omp target teams num_teams(1)
-  !$omp parallel num_threads(1)
-    print *, l
-  !$omp end parallel
-  !$omp end target teams
-
-end program hello_gpu
+end program io_test
